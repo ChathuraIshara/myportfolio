0a27d0e365913f909ec8a0df34baafca9e203d3f
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-05 19:26:26 +0530

    themes initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import './App.css';
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { darkTheme } from './utils/Themes';
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Hi</p>
+    <ThemeProvider theme={darkTheme}>
+    <div className="App">
+      <Body>portfolio</Body>
     </div>
+    </ThemeProvider>
+    
   );
 }
 
