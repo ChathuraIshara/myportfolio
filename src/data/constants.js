fdbec7e4914d0a2fe2e25e8e6ed81af5a10fcf32
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-07-07 23:42:03 +0530

    project section initialized

--- src/data/constants.js
+++ src/data/constants.js
@@ -323,7 +323,7 @@ export const Bio = {
       date: "August 2023 - Jul 2024",
       description:"The Medicare Hub system is a full-stack web application that optimizes healthcare appointment management. It features robust scheduling, real-time notifications, and data analytics capabilities, aimed at improving patient care coordination and operational efficiency for healthcare providers.",
       image:
-        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/medicarehub1.png",
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/med5.png",
       tags: [
         "Azure",
         "Git",
@@ -343,169 +343,134 @@ export const Bio = {
     },
     {
       id: 0,
-      title: "Podstream",
-      date: "Apr 2023 - May 2023",
+      title: "SmartGrocery",
+      date: "Jan 2022 - May 2022",
       description:
-        "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+        "The SmartGrocery app is a convenient and user-friendly mobile application developed using Flutter. It allows users to easily browse and order a wide range of grocery products from the comfort of their homes. With a seamless interface, users can search for items, add them to their cart, and place orders for home delivery.",
       image:
-        "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-      tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-      category: "web app",
-      github: "https://github.com/rishavchanda/Podstream",
-      webapp: "https://podstream.netlify.app/",
-      member: [
-        {
-          name: "Rishav Chanda",
-          img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-          linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-          github: "https://github.com/rishavchanda/",
-        },
-        {
-          name: "Upasana Chaudhuri",
-          img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-          github: "https://github.com/upasana0710",
-        },
-      ],
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/groc5.png",
+      tags: ["Flutter", "MongoDb"],
+      category: "android app",
+      github: "https://github.com/ChathuraIshara/grocery_store_app",
     },
     {
       id: 1,
-      title: "Vexa",
-      date: "Oct 2022 - Present",
+      title: "Eshopping",
+      date: "Oct 2022 - Dec 2022",
       description:
-        "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+        "The platform allows users to browse products, add items to their cart, and securely complete purchases. It includes features such as user authentication, product search and filtering, and an admin panel for managing inventory and orders. The combination of React's dynamic user interface and Laravel's robust backend capabilities ensures a smooth and efficient shopping experience for users.",
       image:
-        "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/Ecommerce.png",
       tags: [
         "React Js",
-        "MongoDb",
-        "Node Js",
-        "Express Js",
-        "Redux",
-        "NodeMailer",
+        "MSSQL",
+        "Laravel",
+        "PHP",
       ],
       category: "web app",
-      github: "https://github.com/rishavchanda/Project-Management-App",
-      webapp: "https://vexa-app.netlify.app/",
+      github: "https://github.com/ChathuraIshara/Ecommerce-website2",
     },
     {
       id: 2,
-      title: "Brain Tumor Detection",
+      title: "LiveNews",
       date: "Jan 2023 - Mar 2023",
       description:
-        "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+        "The live news mobile app I developed using Flutter delivers real-time news updates by fetching data from a free API. The app provides users with the latest news across various categories, ensuring they stay informed with current events and breaking news as they happen.",
       image:
-        "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
-      tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-      category: "machine learning",
-      github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
-      webapp: "https://brain-tumor.netlify.app/",
-      member: [
-        {
-          name: "Rishav Chanda",
-          img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-          linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-          github: "https://github.com/rishavchanda/",
-        },
-        {
-          name: "Upasana Chaudhuri",
-          img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-          github: "https://github.com/upasana0710",
-        },
-      ],
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/livenews.png",
+      tags: ["Flutter","NewsFreeApi"],
+      category: "android app",
+      github: "https://github.com/ChathuraIshara/live_news_app",
     },
     {
       id: 3,
-      title: "Buckoid",
-      date: "Dec 2021 - Apr 2022",
+      title: "EasyQuiz",
+      date: "Nov 2022 - Dec 2023",
       description:
-        "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
+        "A fullstack web application designed for creating and taking quizzes. Developed using modern web technologies, this app features an intuitive user interface, secure user authentication, and real-time quiz analytics. Users can create, share, and participate in various quizzes, with scores and progress tracked seamlessly.",
       image:
-        "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
-      tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
-      category: "android app",
-      github: "https://github.com/rishavchanda/Buckoid-Android-App",
-      webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/quizapp.png",
+      tags: ["ReactJs", "NodeJs", "MongoDb","Expressjs"],
+      category: "web app",
+      github: "https://github.com/ChathuraIshara/Quiz-App",
     },
     {
       id: 10,
-      title: "Job Finding App",
-      date: "Jun 2023 - Jul 2023",
+      title: "Eclass",
+      date: "Apr 2023 - Aug 2023",
       description:
-        "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
+        "Eclass is a fullstack web application designed for managing student data in a school. Developed with a focus on simplicity and efficiency, Eclass allows administrators and teachers to easily manage student records, including personal information, attendance, and academic performance.",
       image:
-        "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
-      tags: ["React Native", "JavaScript", "Axios"],
-      category: "android app",
-      github: "https://github.com/rishavchanda/Job-finder-App",
-      webapp: "https://github.com/rishavchanda/Job-finder-App",
-    },
-    {
-      id: 4,
-      title: "Whatsapp Clone",
-      date: "Jul 2021",
-      description:
-        "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
-      tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+        "https://raw.githubusercontent.com/ChathuraIshara/myportfolio/main/src/images/studentregister.png",
+      tags: ["ReactJs", "Asp.net", "MSSQL"],
       category: "web app",
-      github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
-      webapp: "https://whatsapp-clone-rishav.web.app",
+      github: "https://github.com/ChathuraIshara/StudentRegisterFullstackwebapp",
     },
-    {
-      id: 5,
-      title: "Todo Web App",
-      date: "Jun 2021",
-      description:
-        " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-      image:
-        "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-      tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-      category: "web app",
-      github: "https://github.com/rishavchanda/Todo-Web-App",
-      webapp: "https://rishav-react-todo.netlify.app/",
-    },
-    {
-      id: 6,
-      title: "Breaking Bad",
-      date: "Jun 2021",
-      description:
-        "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
-      image:
-        "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
-      tags: ["React Js", "API", "Axios", "Node JS"],
-      category: "web app",
-      github: "https://github.com/rishavchanda/Breaking-Bad",
-      webapp: "https://breaking-bad-webapp.netlify.app",
-    },
-    {
-      id: 7,
-      title: "Quiz App",
-      date: "Dec 2020 - Jan 2021",
-      description:
-        "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
-      image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
-      tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
-      category: "android app",
-      github: "https://github.com/rishavchanda/Quiz-Earn",
-      webapp: "https://github.com/rishavchanda/Quiz-Earn",
-    },
-    {
-      id: 8,
-      title: "Face Recognition",
-      date: "Jan 2021",
-      description:
-        "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-      image:
-        "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
-      tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-      category: "machine learning",
-      github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-      webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-    },
+    // {
+    //   id: 4,
+    //   title: "Whatsapp Clone",
+    //   date: "Jul 2021",
+    //   description:
+    //     "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
+    //   image:
+    //     "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
+    //   tags: ["React Js", "Firebase", "Firestore", "Node JS"],
+    //   category: "web app",
+    //   github: "https://github.com/rishavchanda/Whatsapp-Clone-React-Js",
+    //   webapp: "https://whatsapp-clone-rishav.web.app",
+    // },
+    // {
+    //   id: 5,
+    //   title: "Todo Web App",
+    //   date: "Jun 2021",
+    //   description:
+    //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+    //   image:
+    //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+    //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+    //   category: "web app",
+    //   github: "https://github.com/rishavchanda/Todo-Web-App",
+    //   webapp: "https://rishav-react-todo.netlify.app/",
+    // },
+    // {
+    //   id: 6,
+    //   title: "Breaking Bad",
+    //   date: "Jun 2021",
+    //   description:
+    //     "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+    //   image:
+    //     "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+    //   tags: ["React Js", "API", "Axios", "Node JS"],
+    //   category: "web app",
+    //   github: "https://github.com/rishavchanda/Breaking-Bad",
+    //   webapp: "https://breaking-bad-webapp.netlify.app",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Quiz App",
+    //   date: "Dec 2020 - Jan 2021",
+    //   description:
+    //     "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+    //   image:
+    //     "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
+    //   tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
+    //   category: "android app",
+    //   github: "https://github.com/rishavchanda/Quiz-Earn",
+    //   webapp: "https://github.com/rishavchanda/Quiz-Earn",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Face Recognition",
+    //   date: "Jan 2021",
+    //   description:
+    //     "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+    //   image:
+    //     "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
+    //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+    //   category: "machine learning",
+    //   github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    //   webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+    // },
   ];
   
   export const TimeLineData = [
